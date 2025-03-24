dc0a4e658bbe252d6feec730cba828eaf713617c
janinynobrega <janinynobrega@gmail.com>
2025-03-24 16:45:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Menu Mobile
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
